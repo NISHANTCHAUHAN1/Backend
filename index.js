581c4e568b6265541e3e02cfdc08fb4f1debd274
Nishant <nishchaynish8@gmail.com>
2025-09-27 22:56:09 +0530

    index

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get("/",(request,response)=>{
     })
 })
 
-const port = process.env.PORT || 5000;
+const PORT =  process.env.PORT || 8080
 
 
 //using middleweares
@@ -55,7 +55,7 @@ app.use("/api/pin", pinRoutes);
 
 
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
     connectDb();
 })
